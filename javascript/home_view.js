ffd604f6e480a9612313e4bd1e92adf3e971a4ec
Sonita <sonita.borin2@gmail.com>
2024-02-12 12:33:22 +0700

    Init Project

---
+++ javascript/home_view.js
@@ -0,0 +1,20 @@
+// For fixed top header
+const header = document.getElementById('head');
+const adverties = document.getElementById('adv-1'); 
+window.onscroll = () =>{
+    if(window.pageYOffset > (adverties.offsetHeight))
+    {
+        header.style.position="fixed"; 
+        header.style.top = "0"
+        header.style.zIndex="1";
+        header.style.width="100%"
+    }else{
+        header.style.position=""; 
+        header.style.top = ""
+        header.style.zIndex=""
+        header.style.width=""
+    }
+}
+
+// 
+
